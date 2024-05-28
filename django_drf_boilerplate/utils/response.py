64ce322e9f24c037a0f149d44e233f4397a955c0
0xankit <ankit.kr554@gmail.com>
2024-05-28 12:21:59 +0530

    feat: add api response structure

---
+++ django_drf_boilerplate/utils/response.py
@@ -0,0 +1,73 @@
+from django.http import JsonResponse
+from rest_framework import status
+
+
+class ApiResponse:
+    '''
+    Api response class for returning response in standard format
+    '''
+    @staticmethod
+    def success(data=None, message='Success', status_code=status.HTTP_200_OK):
+        """
+        Returns a success response in standard format
+
+        Parameters
+        ----------
+            data : dict
+                data to be sent in response
+            message : str
+                message to be sent in response
+            status_code : int
+                status code of response
+        """
+        response_data = {'success': True, 'message': message}
+        if data is not None:
+            response_data['data'] = data
+        return JsonResponse(response_data, status=status_code)
+
+    @staticmethod
+    def error(message='Error', status_code=status.HTTP_400_BAD_REQUEST):
+        '''
+        Returns an error response in standard format
+
+        Parameters
+        ----------
+            message : `str`
+                message to be sent in response
+            status_code : `int`
+                status code of response
+        '''
+        response_data = {'success': False, 'message': message}
+        return JsonResponse(response_data, status=status_code)
+
+    @staticmethod
+    def exception(message="Exception", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR):
+        '''
+        Returns an exception response in standard format
+
+        Parameters
+        ----------
+            message : `str`
+                message to be sent in response
+            status_code : `int`
+                status code of response
+        '''
+        response_data = {'success': False, 'message': message}
+        return JsonResponse(response_data, status=status_code)
+
+    @staticmethod
+    def custom_response(data, message, status_code):
+        '''
+        Returns a custom response in standard format
+
+        Parameters
+        ----------
+            data : `dict`
+                data to be sent in response
+            message : `str`
+                message to be sent in response
+            status_code : `int`
+                status code of response
+        '''
+        response_data = {'success': True, 'message': message, 'data': data}
+        return JsonResponse(response_data, status=status_code)
